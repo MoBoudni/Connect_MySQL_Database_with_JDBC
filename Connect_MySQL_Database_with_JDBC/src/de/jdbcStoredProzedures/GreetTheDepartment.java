c314b208b6acaecf315313d3dbafe262946e2a9f
Mohamed Boudni <moboudni@gmail.com>
2025-09-01 13:08:14 +0200

    Feature: Mitarbeiterliste nach Abteilung filtern
    - Methode getEmployeesForDepartment hinzugefügt
    - Stored Procedure in DB angelegt
    - Testdaten aktualisiert

---
+++ Connect_MySQL_Database_with_JDBC/src/de/jdbcStoredProzedures/GreetTheDepartment.java
@@ -0,0 +1,58 @@
+package de.jdbcStoredProzedures;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+public class GreetTheDepartment {
+
+	public static void main(String[] args) throws Exception {
+
+		Connection myConn = null;
+		CallableStatement myStmt = null;
+
+		try {
+			// Get a connection to database
+			myConn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/demo", "root", "Chakeb1978&");
+
+			String theDepartment = "Engineering";
+			
+			// Prepare the stored procedure call
+			myStmt = myConn
+					.prepareCall("{call greet_the_department(?)}");
+
+			// Set the parameters
+			myStmt.registerOutParameter(1, Types.VARCHAR);
+			myStmt.setString(1, theDepartment);
+
+			// Call stored procedure
+			System.out.println("Calling stored procedure.  greet_the_department('" + theDepartment + "')");
+			myStmt.execute();
+			System.out.println("Finished calling stored procedure");			
+			
+			// Get the value of the INOUT parameter
+			String theResult = myStmt.getString(1);
+			
+			System.out.println("\nThe result = " + theResult);
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			close(myConn, myStmt);
+		}
+	}
+
+	private static void close(Connection myConn, Statement myStmt) throws SQLException {
+		if (myStmt != null) {
+			myStmt.close();
+		}
+
+		if (myConn != null) {
+			myConn.close();
+		}
+	}
+}
---
+++ Connect_MySQL_Database_with_JDBC/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Connect_MySQL_Database_with_JDBC {
+	requires java.sql;
+}
